d6dcd81c72a95c3711c94385c25781ebe0c767bc
chinons098 <chinonsoakolam@outlook.com>
2025-02-08 14:07:16 -0500

    clippy still in prog

--- src/app/system-apps/clippy/clippy.component.ts
+++ src/app/system-apps/clippy/clippy.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { RunningProcessService } from 'src/app/shared/system-service/running.process.service';
 import { BaseComponent } from 'src/app/system-base/base/base.component';
 import { ComponentType } from 'src/app/system-files/component.types';
@@ -14,27 +14,35 @@ import { Process } from 'src/app/system-files/process';
 })
 export class ClippyComponent implements BaseComponent, OnInit, OnDestroy, AfterViewInit {
 
+  @ViewChild('clippyToolTip', {static: true}) clippyToolTip!: ElementRef;
+  @ViewChild('clippyToolTipText', {static: true}) clippyToolTipText!: ElementRef;
+  @ViewChild('clippyGifFigure', {static: true}) clippyGifFigure!: ElementRef; 
+
   private _runningProcessService:RunningProcessService;
 
-  
+  gifPath = `${Constants.GIF_BASE_PATH}clippy_hey_you.gif`;
+  SHOW_TOOL_TIP_DELAY = 500; 
+  MAX_TOOL_TIP_DISPLAY_DURATION =4500; 
+  MIN_GIF_DISPLAY_DURATION = 6000;
+  PID = 20000;
+  clippyDurations:number[] = [4400,2400,13600,7500,1800,5500,8400,4100,6600,2200,3500,2800,3000,3000,5000,4500,1900,2600,8100,4800];
+
+  clippyAnimations:string[] = ['clippy_correct','clippy_listen_music','clippy_relax','clippy_melt','clippy_look_down','clippy_boxed',
+    'clippy_silly','clippy_goodbye','clippy_reading','clippy_point_here','clippy_hi_there','clippy_point_up','clippy_point_right',
+    'clippy_point_left','clippy_file_vortex', 'clippy_atomic','clippy_puzzled','clippy_hey_you','clippy_searching','clippy_no'];
+
+  clippyTextTips:string[] = ['Do not interrupt me!!'];
+
+
+  isToolTipVisible = false;
+  toolTipText = 'This is a test of the emergency broadcast system';
+
   name= 'clippy';
   hasWindow = false;
-  
   icon = `${Constants.IMAGE_BASE_PATH}generic_program.png`;
-  PID = 20000;
   processId = 0;
   type = ComponentType.User;
   displayName = Constants.EMPTY_STRING;
-
-
-
-  gifPath = `${Constants.GIF_BASE_PATH}clippy_searching.gif`;
-  MIN_DURATION = 6000;
-  clippyDurations:number[]  = [4400,2400,13600,7500,1800,5500,8400,4100,6600,2200,3500,2800,3000,3000,5000,4500,1900,2600,8100,4800];
-
-  clippyAnimations:string[]  = ['clippy_correct','clippy_listen_music','clippy_relax','clippy_melt','clippy_look_down','clippy_boxed',
-    'clippy_silly','clippy_goodbye','clippy_reading','clippy_point_here','clippy_hi_there','clippy_point_up','clippy_point_right',
-    'clippy_point_left','clippy_file_vortex', 'clippy_atomic','clippy_puzzled','clippy_hey_you','clippy_searching','clippy_no'];
 
   constructor(runningProcessService:RunningProcessService) {       
     this._runningProcessService = runningProcessService;
@@ -43,17 +51,50 @@ export class ClippyComponent implements BaseComponent, OnInit, OnDestroy, AfterV
   }
   
 
-
   ngOnInit(): void {
    1
   }
 
   ngAfterViewInit():void{   
-    1
+    this.showClippyToolTip();
   }
 
   ngOnDestroy():void{
     1
+  }
+
+  onToolTipCntnrClick():void{
+    this.toolTipText = this.clippyTextTips[0];
+    this.gifPath = `${Constants.GIF_BASE_PATH}clippy_no.gif`;
+  }
+
+  onClippyGifCntnrClick():void{
+    this.toolTipText = this.clippyTextTips[0];
+    this.gifPath = `${Constants.GIF_BASE_PATH}clippy_no.gif`;
+  }
+
+  private showClippyToolTip():void{
+
+    setTimeout(()=>{
+      this.clippyToolTip.nativeElement.style.visibility = 'visible';
+      this.clippyToolTip.nativeElement.style.opacity = 1;
+      this.clippyToolTip.nativeElement.style.transition = 'opacity 0.3s ease-in';
+
+      this.clippyToolTipText.nativeElement.style.visibility = 'visible';
+      this.clippyToolTipText.nativeElement.style.opacity = 1;
+      this.clippyToolTipText.nativeElement.style.transition = 'opacity 0.3s ease-in';
+
+    },this.SHOW_TOOL_TIP_DELAY) 
+  }
+
+  private hideClippyToolTip():void{
+    this.clippyToolTip.nativeElement.style.visibility = 'hidden';
+    this.clippyToolTip.nativeElement.style.opacity = 0;
+    this.clippyToolTip.nativeElement.style.transition = 'opacity 0.3s ease-out';
+
+    this.clippyToolTipText.nativeElement.style.visibility = 'hidden';
+    this.clippyToolTipText.nativeElement.style.opacity = 0;
+    this.clippyToolTipText.nativeElement.style.transition = 'opacity 0.3s ease-out';
   }
   
   private getComponentDetail():Process{
