e61ff8cb09ab9c925183673dfb888c14793d5918
Chinonso <chinonsoakolam@outlook.com>
2025-02-13 14:59:04 -0500

    chat object added
    chat object added

---
+++ src/app/system-apps/chatter/chat.message.ts
@@ -0,0 +1,40 @@
+export class ChatMessage {
+    private _msg: string;
+    private _userName:String;
+    private _msgOutput: string;
+    private _msgDate:string;
+
+    constructor(msg:string, userName:string, msgOutput:string ){
+        this._msg = msg;
+        this._userName = userName;
+        this._msgOutput = msgOutput;
+
+        const dateTime = new Date();  
+        this._msgDate = `${dateTime.getMonth() + 1}/${dateTime.getDate()}/${dateTime.getFullYear()}`;
+    }
+
+    get getMessage(){
+        return this._msg;
+    }
+    set setMessage(msg:string){
+         this._msg = msg;
+    }
+
+    get getUserName(){
+        return this._userName;
+    }
+    set setUserName(userName:string){
+        this._userName= userName
+    }
+
+    get getMessageOutput(){
+        return this._msgOutput;
+    }
+    set setMessageOutput(cmdOutput:string){
+        this._msgOutput = cmdOutput;
+    }
+
+    get getMsgDate(){
+        return this._msgDate;
+    }
+  }
